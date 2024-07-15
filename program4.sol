d4438517e3fcb56b9eeaeeb23097cc73c5ce0392
okabhishek88 <okabhishek88@gmail.com>
2024-07-15 21:52:21 +0530

    program4

---
+++ program4.sol
@@ -0,0 +1,17 @@
+// Write a program to swap two numbers using a third variable
+
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Average {
+    function f1(uint256 value1, uint256 value2) public pure returns (uint256, uint256){
+        uint256 value3; // third variable
+
+        value3 = value1;
+        value1 = value2;
+        value2 = value3;
+
+        return (value1, value2);
+    }
+}
