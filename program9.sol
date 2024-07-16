cd080cbbcb5a4e22ca206e3f2a156f35f147d962
okabhishek88 <okabhishek88@gmail.com>
2024-07-16 20:56:06 +0530

    program9

---
+++ program9.sol
@@ -0,0 +1,30 @@
+// Write a program to check if a given number is armstrong number or not
+
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract isArmstrongNumber {
+    function f1(uint256 value1, uint value2, uint value3) public pure returns (string memory) {
+        
+        if (value1 > value2){
+
+            if (value1 > value3){
+                return "value1 is greater";
+            }
+
+            else {
+                return "value3 is greater";
+            }
+            
+        }
+
+        else if (value2 > value3){
+            return "value2 is greater";
+        }
+
+        else{
+            return "Value3 is greater";
+        }
+    }
+}
