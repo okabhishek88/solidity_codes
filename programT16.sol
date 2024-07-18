f5a70fa0c029d5d8218ea04c0b5c296f680754f7
okabhishek88 <okabhishek88@gmail.com>
2024-07-18 21:06:14 +0530

    program16

---
+++ programT16.sol
@@ -0,0 +1,18 @@
+// Write a program to Print Fibonacci Series
+
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+contract NaturalNum {
+    function f1(uint256 value) public pure returns (uint256) {
+        require(value > 0, "value must be a positive number and greater than 1");
+
+        uint sum = 0;
+
+        for (uint i = 1; i <= value; i++){
+            sum = sum + i;
+        }
+
+        return sum;
+    }
+}
